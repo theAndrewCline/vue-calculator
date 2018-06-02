1771f6ccf1fb809344863daf272382371608db85
theAndrewCline <andrew.cline77@gmail.com>
2018-06-02 11:01:14 -0500

    changes to css

--- js/app.js
+++ js/app.js
@@ -1,32 +1,29 @@
 const app = new Vue({
-    el:'#root',
-    data: {
-        value: 0,
-        equation: ''
-    },
-    methods: {
-        add: function() {
-
-        },
-        subract: function() {
-
-        },
-        multiply: function() {
-
-        },
-        divide: function() {
-
-        },
-        changeValue: function(event) {
-
-            if ( this.value === 0) {
-                this.value = Number(event.target.id)
-            } else {
-                this.value= Number(this.value.toString() + event.target.id)
-            }
-            console.log(this.value)
-        }
-
-    }
-
+	el: '#root',
+	data: {
+		value: 0,
+		equation: ''
+	},
+	methods: {
+		add: function() {
+			console.log('add')
+		},
+		subtract: function() {
+			console.log('subtract')
+		},
+		multiply: function() {
+			console.log('multiply')
+		},
+		divide: function() {
+			console.log('divide')
+		},
+		changeValue: function(event) {
+			if (this.value === 0) {
+				this.value = Number(event.target.id)
+			} else {
+				this.value = Number(this.value.toString() + event.target.id)
+			}
+			console.log(this.value)
+		}
+	}
 })
